67d5afa502b294203aff9c291669fd05a57032be
Shahed_ali <mohammedshahedali464@gmail.com>
2020-12-10 22:45:45 +0530

    Code completed Dec-10-2020

---
+++ Size_of.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void main() {
+	cout << "Welcome I'm Mohammed Shahed Ali" << endl;
+	cout << "Here we will find out size of different data types" << endl;
+	cout << "Size of int" << sizeof(int) << endl;
+	cout << "Size of char" << sizeof(char) << endl;
+	cout << "Size of float" << sizeof(float) << endl;
+	cout << "Size of double" << sizeof(double) << endl;
+}
---
+++ Source1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main() {
+	cout << "Welcome I'm Mohammed Shahed Ali"<<endl;
+	cout << "Here we will find out size of different data types" << endl;
+	cout << "Size of int" << sizeof(int) << endl;
+	cout << "Size of char" << sizeof(char) << endl;
+	cout << "Size of float" << sizeof(float) << endl;
+	cout << "Size of double" << sizeof(double) << endl;
+	cout << "Size of unsigned int" << sizeof(unsigned int) << endl;
+	cout << "Size of unsigned char " << sizeof(unsigned char) << endl;
+}
